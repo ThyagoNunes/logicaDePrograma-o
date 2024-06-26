1a1523d2eb3ca1a703e8e942a702f3dc98b1a388
Thyago Nunes <devthyagonunes@gmail.com>
2024-06-26 12:54:37 -0300

    teste

--- totalNegativeNumbers.js
+++ totalNegativeNumbers.js
@@ -1,6 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -9, -8, -7, -6, -5, -4, -3, -2, -1,]
 
-
 function totalNumbersNegative(numbers) {
   let storageNegativeNumbers = []
   let countNegativeNumbers = 0;
