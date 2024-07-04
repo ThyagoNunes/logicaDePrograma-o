c0526154cb87d9a4b35bdfd260664a96123d9671
Thyago Nunes <devthyagonunes@gmail.com>
2024-07-04 20:29:27 -0300

    single responsability

---
+++ mergeSortSingleResponsability.js
@@ -0,0 +1,22 @@
+const merge = require('./merge')
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right)
+}
+
+
+
+
+
+
+const arr = [82, 15, 43, 97, 69, 34, 6, 91, 20, 3, 50, 72, 86, 59, 24, 77, 10, 39, 18, 93, 55, 12, 88, 37, 65, 52, 29, 84, 47, 99];
+console.log("Original array:", arr);
+console.log("Sorted array:", mergeSort(arr));
