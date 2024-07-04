c48c5d00d1553dfd3d799882c34f0fe9fd144125
Thyago Nunes <devthyagonunes@gmail.com>
2024-07-04 20:31:26 -0300

    remove blank spaces

--- mergeSortSingleResponsability.js
+++ mergeSortSingleResponsability.js
@@ -12,11 +12,6 @@ function mergeSort(arr) {
   return merge(left, right)
 }
 
-
-
-
-
-
 const arr = [82, 15, 43, 97, 69, 34, 6, 91, 20, 3, 50, 72, 86, 59, 24, 77, 10, 39, 18, 93, 55, 12, 88, 37, 65, 52, 29, 84, 47, 99];
 console.log("Original array:", arr);
 console.log("Sorted array:", mergeSort(arr));
