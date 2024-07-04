b9a8136fb1e1f14ecece4109e3bf2dd663fd67a0
Thyago Nunes <devthyagonunes@gmail.com>
2024-07-04 18:13:38 -0300

    Algoritmo de ordenação Merge Sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,31 @@
+/* 
+  Essa ordenação é por meio de quebra do array ao meio e usado recursividade até que exista apenas uma única fração contendo um elemento
+  [9, 3, 0, 5, 8, 4]
+  1 [9,3,0]
+    1.1 [9] [3,0]
+      1.2 [9] [3] [0]
+        1.3 [9] compare [3] e [0]
+          1.4 [3] > [0]
+            1,5 [3,0]
+              1.6 [9] > [0,3]
+                1.7 [0, 3, 9]
+  2 [5,8,4]
+   2.1 [5] [8,4]
+      2.2 [5] [8] [4]
+        2.3 [5] compare [8] e [4]
+          2.4 [8] > [4]
+            2,5 [8,4]
+              2.6 [5] > [4,8]
+                2.7 [4, 5, 8]
+  3 [0, 3, 9] [4, 5, 8]
+    3,1 0 > 4 ? [0, ]
+      3.2 3 > 4 ? [0, 3]
+        3.3 9 > 4 ? [0, 3, 4]
+          3.4 9 > 5 ? [0, 3, 4, 5]
+            3.5 9 > 8 ? [0, 3, 4, 5, 8]
+              3.6 [0, 3, 4, 5, 8, 9] 
+*/
+
 function mergeSort(arr) {
     if (arr.length <= 1) {
         return arr;
@@ -11,7 +39,6 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
-
     let result = [];
     let leftIndex = 0;
     let rightIndex = 0;
@@ -24,14 +51,12 @@ function merge(left, right) {
             result.push(right[rightIndex]);
             rightIndex++;
         }
-        console.log(`left: ${left}` )
-  console.log(`right: ${right}` )
     }
     
-    // Add remaining elements
+    // Aqui eu construo o array com os elementos ordenados da esquerda e direita 
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
-const arr = [5, 2, 9, 1, 5, 6, 10];
+const arr = [82, 15, 43, 97, 69, 34, 6, 91, 20, 3, 50, 72, 86, 59, 24, 77, 10, 39, 18, 93, 55, 12, 88, 37, 65, 52, 29, 84, 47, 99];
 console.log("Original array:", arr);
 console.log("Sorted array:", mergeSort(arr));
